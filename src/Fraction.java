7ad6ed69fc0dfab991639d41b928f568d1fa6cba
Mgmaddox05 <125920278+Mgmaddox05@users.noreply.github.com>
2023-08-30 15:17:02 -0500

    New Program

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,3 +1,4 @@
+import java.io.*;
 public class Fraction {
     private int numerator;
     private int denominator;
@@ -12,17 +13,56 @@ public class Fraction {
     {
         this.numerator = numerator;
         this.denominator = denominator;
+        
     }
 
     public Fraction Add(Fraction other)
     {
-        int numerator = this.numerator + other.numerator;
-        Fraction fracSum = new Fraction();
+       
+        // Finding LCM
+        int denominator2 = lcm(this.denominator, other.denominator);
+        
+        //Adding Numerators
+        int multiply = denominator2/this.denominator;
+        int multiply2 = denominator2/other.denominator;
+        int newNum1 = this.numerator * multiply;
+        int newNum2 = other.numerator * multiply2;
+        int numerator = newNum1 + newNum2;
+        
+        Fraction fracSum = new Fraction(numerator, denominator);
+        
+        fracSum.reduce();
+        
+         
         return fracSum;
     }
 
-    public String ToString()
+    public String toString()
     {
         return numerator + " " + "/" + " " + denominator;
     }
+    
+    private void reduce()
+    {
+       int divisior = gcd(numerator, denominator);
+       System.out.println(divisior + " Div"); 
+        numerator = numerator/divisior;
+       denominator = denominator/divisior;
+        
+    }
+
+    static int gcd(int a, int b)
+    {
+        if(b == 0)
+        {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+     
+    static int lcm(int a, int b)
+    {
+        return (a / gcd(a, b)) * b;
+    }
+        
 }
